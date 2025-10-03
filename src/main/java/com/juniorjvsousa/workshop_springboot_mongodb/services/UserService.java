04a5a06846a958eb305b0b7e901e01d14536f41e
Junior Sousa <jose.valberto@maisunifacisa.com.br>
2025-10-03 16:42:07 -0300

    OInserção de usuário com POST

--- src/main/java/com/juniorjvsousa/workshop_springboot_mongodb/services/UserService.java
+++ src/main/java/com/juniorjvsousa/workshop_springboot_mongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.juniorjvsousa.workshop_springboot_mongodb.services;
 
 import com.juniorjvsousa.workshop_springboot_mongodb.domain.User;
+import com.juniorjvsousa.workshop_springboot_mongodb.dto.UserDTO;
 import com.juniorjvsousa.workshop_springboot_mongodb.repository.UserRepository;
 import com.juniorjvsousa.workshop_springboot_mongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,4 +25,17 @@ public class UserService {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+    }
+
+
+
+
+
 }
