dd26a6cf6189e4ab031a3bafba24ad4c4b835021
Junior Sousa <jose.valberto@maisunifacisa.com.br>
2025-10-03 16:52:56 -0300

    PUT

--- src/main/java/com/juniorjvsousa/workshop_springboot_mongodb/resources/UserResource.java
+++ src/main/java/com/juniorjvsousa/workshop_springboot_mongodb/resources/UserResource.java
@@ -47,4 +47,13 @@ public class UserResource {
         return ResponseEntity.noContent().build();
 
     }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+
+    }
 }
